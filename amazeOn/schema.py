ccc4fc5e90e5bc99682ed3b4c3de24590bc96578
Periketi Ravi Teja <periketi.teja@In-Periketi.local>
2020-05-28 02:00:51 +0530

    Added pagination and documented meta queries

--- amazeOn/schema.py
+++ amazeOn/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import time
 
 from graphene_django.types import DjangoObjectType
 
@@ -7,6 +8,12 @@ from review.models import Review
 
 
 class ProductType(DjangoObjectType):
+    delayed_field = graphene.String()
+
+    def resolve_delayed_field(self, info):
+        time.sleep(4)
+        return 'delayed text'
+
     class Meta:
         model = Product
 
@@ -17,17 +24,35 @@ class ReviewType(DjangoObjectType):
 
 
 class Query(object):
-    all_products = graphene.List(ProductType)
-    all_reviews = graphene.List(ReviewType)
+    all_products = graphene.List(
+        ProductType,
+        pagesize=graphene.Int(),
+        offset=graphene.Int(),
+    )
+    all_reviews = graphene.List(
+        ReviewType,
+        pagesize=graphene.Int(),
+        offset=graphene.Int(),
+    )
 
     product = graphene.Field(ProductType, id=graphene.Int())
     review = graphene.Field(ReviewType, id=graphene.Int())
 
-    def resolve_all_products(self, info, **kwargs):
-        return Product.objects.all()
+    def resolve_all_products(self, info, pagesize=None, offset=0, **kwargs):
+        query_set = Product.objects.all()
 
-    def resolve_all_reviews(self, info, **kwargs):
-        return Review.objects.select_related('product').all()
+        if pagesize is not None:
+            return query_set[offset: offset + pagesize]
+
+        return query_set
+
+    def resolve_all_reviews(self, info,pagesize=None, offset=0, **kwargs):
+        query_set = Review.objects.select_related('product').all()
+
+        if pagesize is not None:
+            return query_set[offset: offset + pagesize]
+
+        return query_set
 
     def resolve_product(self, info, **kwargs):
         product_id = kwargs.get('id')
